3c6acbacdb70f65e3587e5759dfa7fe817162b41
Daneev <30390132+Daneev@users.noreply.github.com>
2018-01-22 00:15:17 +0400

    Замечания отработаны.

--- pprint_json.py
+++ pprint_json.py
@@ -11,14 +11,19 @@ def load_data(filepath):
         return json.load(file)
 
 
-def pretty_print_json(raw_json):
-    print(json.dumps(raw_json, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(parent_data):
+    print(json.dumps(parent_data, sort_keys=True, indent=4, ensure_ascii=False))
     return None
 
 
-if __name__ == '__main__':
+def get_path_data():
     if len(argv) > 1:
-        pretty_print_json(load_data(argv[1]))
+        return argv[1]  # возвращаем путь к файлу, если программу запустили с параметрами
     else:
-        print("Введите путь до файла с произвольными данными в формате JSON")
-        pretty_print_json(load_data(input()))
+        print("Введите путь до файла формата JSON")
+        return input()  # если программа запущена без параметров,
+        # ожидаем ввода пути к файлу с клавиатуры.
+
+
+if __name__ == '__main__':
+    pretty_print_json(load_data(get_path_data()))
