bdda7b8d5a7e3c21c317903a37ae08d747e04701
Daneev <30390132+Daneev@users.noreply.github.com>
2018-01-22 23:19:25 +0400

    упростил код

--- pprint_json.py
+++ pprint_json.py
@@ -1,29 +1,28 @@
+import json
+import argparse
 from sys import argv
-import json, os
 
 
 def load_data(filepath):
-    if not os.path.exists(filepath):
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
         print("There is no file in the specified path.")
-        return None
-
-    with open(filepath, 'r', encoding='utf-8') as file:
-        return json.load(file)
-
+        quit()
 
 def pretty_print_json(parent_data):
     print(json.dumps(parent_data, sort_keys=True, indent=4, ensure_ascii=False))
-    return None
 
 
 def get_path_data():
     if len(argv) > 1:
-        return argv[1]  # возвращаем путь к файлу, если программу запустили с параметрами
+        return argv[1]
     else:
         print("Введите путь до файла формата JSON")
-        return input()  # если программа запущена без параметров,
-        # ожидаем ввода пути к файлу с клавиатуры.
+        return input()
 
 
 if __name__ == '__main__':
     pretty_print_json(load_data(get_path_data()))
+
